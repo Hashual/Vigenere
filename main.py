dfe62f2386d48af5c4202f2be017b4874a56fe5e
Hashual <loizeau.dorian05@gmail.com>
2024-10-04 19:20:11 +0200

    Refacto : extract main method to another file

---
+++ main.py
@@ -0,0 +1,28 @@
+from ChiffrementVigenere import *
+
+if __name__ == "__main__" :
+    print("MENU :")
+    print("1. Chiffrer un message")
+    print("2. Déchiffrer un message")
+    print("3. Trouver la taille d'une clé dans un fichier")
+    print("4. Quitter")
+    choix = input()
+    if choix == "1":
+        texte = input("Entrez le message à chiffrer : ")
+        cle = input("Entrez la clé de chiffrement : ")
+        print("Votre message chiffré : " + chiffreVigenere(texte, cle))
+
+    elif choix == "2":
+        texte = input("Entrez le message à déchiffrer : ")
+        cle = input("Entrez la clé de déchiffrement : ")
+        print("Votre message déchiffré : " + dechiffreVigenere(texte, cle))
+
+    elif choix == "3":
+        chemin_fichier = input("Entrez le chemin du fichier à lire : \n")
+        methodeKasiski(chemin_fichier)
+
+    elif choix == "4":
+        print("Au revoir !")
+
+    else:
+        print("Choix invalide")
